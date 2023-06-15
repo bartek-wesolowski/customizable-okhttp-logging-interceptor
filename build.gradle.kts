484779270840133e70f3bd58195eb0dbfb3dff9b
Bartosz Wesołowski <bartas.wesolowski@gmail.com>
2023-06-15 09:04:18 +0200

    Use gradle-maven-publish-plugin

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("com.vanniktech.maven.publish") version "0.25.2"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+dependencies {
+    api("com.squareup.okhttp3:okhttp:4.11.0")
+}
+
+// https://vanniktech.github.io/gradle-maven-publish-plugin/central/
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.S01, true)
+    signAllPublications()
+
+    coordinates("io.github.bartek-wesolowski", "mockito-kotlin-rx", "2.0.0")
+
+    pom {
+        name.set("Customizable OkHttp Logging Interceptor")
+        description.set("An OkHttp interceptor which logs HTTP request and response data and allows output customization.")
+        inceptionYear.set("2021")
+        url.set("https://github.com/bartek-wesolowski/customizable-okhttp-logging-interceptor")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("bartek-wesolowski")
+                name.set("Bartosz Wesołowski")
+                url.set("https://github.com/bartek-wesolowski/")
+            }
+        }
+        scm {
+            url.set("https://github.com/bartek-wesolowski/customizable-okhttp-logging-interceptor")
+            connection.set("scm:git:git://github.com/bartek-wesolowski/customizable-okhttp-logging-interceptor.git")
+            developerConnection.set("scm:git:ssh://git@github.com:bartek-wesolowski/customizable-okhttp-logging-interceptor.git")
+        }
+    }
+}
